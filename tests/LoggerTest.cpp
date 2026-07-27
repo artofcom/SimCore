5dcd4d4653337ea5e9fe690fa05429d52baf71b1
Hyungtae <artofcom2@gmail.com>
2026-07-27 14:02:46 -0500

    Added test logger.

--- tests/LoggerTest.cpp
+++ tests/LoggerTest.cpp
@@ -1,6 +1,11 @@
 #include <gtest/gtest.h>
+#include "../include/Simulation.h"
 
 TEST(MathTest, Add)
 {
-    EXPECT_EQ(1 + 1, 2);
+    // EXPECT_EQ(1 + 1, 2);
+
+    Simulation sim;
+    sim.Update(0.1f);
+    SUCCEED();
 }
